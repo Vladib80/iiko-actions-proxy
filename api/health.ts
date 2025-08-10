649c4b5200a9557430772797b91008739cde4259
Vladib80 <130882109+Vladib80@users.noreply.github.com>
2025-08-10 14:15:35 +0500

    Delete api directory

--- api/health.ts
+++
@@ -1,4 +0,0 @@
-export default async function handler(_req, res) {
-  res.setHeader('content-type', 'application/json');
-  res.status(200).send({ status: 'ok', time: new Date().toISOString() });
-}
